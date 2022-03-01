565e5eda6306a19a1dd567cc12a36c0983d088f3
Pablovide <pabloviturrod@gmail.com>
2022-03-01 21:00:34 +0100

    home add to cart

--- reto2/src/main/java/com/example/reto2/Web/API/OrderController.java
+++ reto2/src/main/java/com/example/reto2/Web/API/OrderController.java
@@ -48,119 +48,35 @@ public class OrderController {
 
     @GetMapping("/full/{id}")
     public FullOrder getFullOrderDataByOrderId(@PathVariable Long id) throws Exception {
-        FullOrder fullOrder = new FullOrder();
-        var order = orderService.findById(id);
-        if (order == null) {
+        try {
+            return orderService.getFullOrderByOrderId(id);
+        } catch (Exception e) {
             throw new ObjectNotFoundException(id, "Order");
         }
-        fullOrder.setOrder(order);
-        try {
-            fullOrder.setProducts(getAllProductsByOrderId(id));
-        } catch (Exception e) {
-            throw new ObjectNotFoundException(id, "Products");
-        }
-        return fullOrder;
     }
 
     @PostMapping("")
-    public Long createOrder(@RequestBody MakeOrder makeOrder) {
-        OrderDTO order = new OrderDTO();
-        order.setCustomerName(makeOrder.getCustomerName());
-        order.setCreationDate(Calendar.getInstance().getTime());
-        order.setStatus(OrderStatus.PENDING);
-        order = orderService.add(order);
-        HashMap<Long, Integer> productHMap = mapProductListToHashMap(makeOrder.getProducts());
-        for (var product : productHMap.entrySet()) {
-            OrderProductDTO orderProduct = new OrderProductDTO();
-            orderProduct.setOrderId(order.getId());
-            orderProduct.setProductId(product.getKey());
-            orderProduct.setQuantity(product.getValue());
-            orderProductService.add(orderProduct);
-        }
-        return order.getId();
+    public OrderDTO createOrder(@RequestBody MakeOrder makeOrder) {
+        return orderService.createOrder(makeOrder);
     }
 
     @PutMapping("/add/{id}")
-    public void addProductToOrder(@PathVariable Long id, @RequestParam Long productId, @RequestParam(defaultValue = "1") Integer quantity)
+    public void addProductToOrder(@PathVariable Long id, @RequestParam Long productId,
+            @RequestParam(defaultValue = "1") Integer quantity)
             throws Exception {
-        var orderProducts = orderProductService.findByOrderId(id);
-        if (orderProducts == null) {
-            throw new ObjectNotFoundException(id, "Order");
-        }
-        if (productService.findById(productId) == null) {
-            throw new ObjectNotFoundException(productId, "Product");
-        }
-        for (var orderProduct : orderProducts) {
-            if (orderProduct.getProductId() == productId) {
-                orderProduct.setQuantity(orderProduct.getQuantity() + quantity);
-                orderProductService.update(orderProduct.getId(), orderProduct);
-                return;
-            }
-        }
-        OrderProductDTO orderProductDTO = new OrderProductDTO();
-        orderProductDTO.setOrderId(id);
-        orderProductDTO.setProductId(productId);
-        orderProductDTO.setQuantity(quantity);
-        orderProductService.add(orderProductDTO);
+        orderService.addProductToOrder(id, productId, quantity);
+
     }
 
     @PutMapping("/remove/{id}")
-    public void removeProductFromOrder(@PathVariable Long id, @RequestParam Long productId, @RequestParam(defaultValue = "1") Integer quantity)
+    public void removeProductFromOrder(@PathVariable Long id, @RequestParam Long productId,
+            @RequestParam(defaultValue = "1") Integer quantity)
             throws Exception {
-        var orderProducts = orderProductService.findByOrderId(id);
-        if (orderProducts == null) {
-            throw new ObjectNotFoundException(id, "Order");
-        }
-        if (productService.findById(productId) == null) {
-            throw new ObjectNotFoundException(productId, "Product");
-        }
-        for (var orderProduct : orderProducts) {
-            if (orderProduct.getProductId() == productId) {
-                if (orderProduct.getQuantity() - quantity < 0) {
-                    throw new Exception("Quantity is not valid");
-                }
-                orderProduct.setQuantity(orderProduct.getQuantity() - quantity);
-                orderProductService.update(orderProduct.getId(), orderProduct);
-                return;
-            }
-        }
-        throw new Exception("Product not found");
+        orderService.removeProductFromOrder(id, productId, quantity);
     }
 
     @DeleteMapping("/{id}")
     public void deleteOrder(@PathVariable Long id) {
-        orderService.delete(id);
-        var orderProducts = orderProductService.findByOrderId(id);
-        for (var orderProduct : orderProducts) {
-            orderProductService.delete(orderProduct.getId());
-        }
-    }
-
-    private Object[][] getAllProductsByOrderId(Long id) {
-        var orderProducts = orderProductService.findByOrderId(id);
-        var iterator = -1;
-        var products = new Object[orderProducts.size()][2];
-        for (OrderProductDTO orderProduct : orderProducts) {
-            iterator++;
-            var product = productService.findById(orderProduct.getProductId());
-            if (product == null) {
-                throw new ObjectNotFoundException(orderProduct.getProductId(), "Product");
-            }
-            products[iterator][0] = product;
-            products[iterator][1] = orderProduct.getQuantity();
-        }
-        return products;
-    }
-
-    private HashMap<Long, Integer> mapProductListToHashMap(ArrayList<ProductDTO> products) {
-        HashMap<Long, Integer> productHMap = new HashMap<>();
-        for (ProductDTO product : products) {
-            if (productHMap.containsKey(product.getId())) {
-                productHMap.put(product.getId(), productHMap.get(product.getId()) + 1);
-            } else {
-                productHMap.put(product.getId(), 1);
-            }
-        }
-        return productHMap;
+        orderService.deleteFullOrder(id);
     }
 }
--- reto2/src/main/java/com/example/reto2/Web/Config/InitDB.java
+++ reto2/src/main/java/com/example/reto2/Web/Config/InitDB.java
@@ -19,22 +19,23 @@ public class InitDB {
     @Bean
     CommandLineRunner initDatabase(OrderRepository orderRepos, ProductRepository productRepos, OrderProductRepository orderProductRepos){
         return args -> {
-            orderRepos.save(new OrderEntity(Calendar.getInstance().getTime(),"Matthus Manntschke", OrderStatus.CANCELLED.toString()));
-            orderRepos.save(new OrderEntity(Calendar.getInstance().getTime(),"Avis Lovewell", OrderStatus.DELIVERED.toString()));
-            orderRepos.save(new OrderEntity(Calendar.getInstance().getTime(),"Marthe Jerson", OrderStatus.CANCELLED.toString()));
-            orderRepos.save(new OrderEntity(Calendar.getInstance().getTime(),"Xerxes Lapham", OrderStatus.SHIPPED.toString()));
-            orderRepos.save(new OrderEntity(Calendar.getInstance().getTime(),"Brandice Loughhead", OrderStatus.ACCEPTED.toString()));
-            productRepos.save(new ProductEntity("TV Set", 300, "https://www.amazon.es/Hisense-UHD-2020-43AE7000F-Resoluci%C3%B3n/dp/B08553S69P/ref=sr_1_7?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&keywords=televisor&qid=1639057872&sr=8-7&th=1"));
-            productRepos.save(new ProductEntity("Game Console", 200, "https://www.amazon.es/Nintendo-Switch-Lite-Consola-Estandar/dp/B07V5JJHK4/ref=sr_1_9?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=379R66AG2DEWV&keywords=nintendo+switch&qid=1639057941&sprefix=nintendo+swi%2Caps%2C180&sr=8-9"));
-            productRepos.save(new ProductEntity("Sofa", 100, "https://www.amazon.es/Miriam-Sof%C3%A1-Esquina-Color-Burdeos/dp/B08H99PYKT/ref=sr_1_12?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&keywords=sofa&qid=1639057890&sr=8-12&th=1"));
-            productRepos.save(new ProductEntity("Icecream", 5, "https://www.amazon.es/Ben-Jerrys-Cookie-Dough-Helado/dp/B002VLAS6M/ref=sr_1_8_0o_DIA?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&keywords=helado&qid=1639057963&sr=8-8"));
-            productRepos.save(new ProductEntity("Beer", 3, "https://www.amazon.es/Potter-Cerveza-Mantequilla-Alcohol-Horrocrux/dp/B07M61MT8F/ref=sr_1_3?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&keywords=cerveza&qid=1639058003&refinements=p_36%3A831296031&rnid=831274031&s=grocery&sr=1-3"));
-            productRepos.save(new ProductEntity("Phone", 500, "https://www.amazon.es/Plateado-Pantalla-almacenamiento-cu%C3%A1druple-garant%C3%ADa/dp/B08HJGQ9ZS/ref=sr_1_4?keywords=movil&qid=1639058031&refinements=p_36%3A45000-55000&rnid=1323854031&sr=8-4&th=1"));
-            orderProductRepos.save(new OrderProductEntity(1L, 8L, 8));
-            orderProductRepos.save(new OrderProductEntity(2L, 6L, 1));
-            orderProductRepos.save(new OrderProductEntity(3L, 7L, 10));
-            orderProductRepos.save(new OrderProductEntity(4L, 11L, 3));
-            orderProductRepos.save(new OrderProductEntity(5L, 10L, 20));
+            // orderRepos.save(new OrderEntity(Calendar.getInstance().getTime(),"Matthus Manntschke", OrderStatus.CANCELLED.toString()));
+            // orderRepos.save(new OrderEntity(Calendar.getInstance().getTime(),"Avis Lovewell", OrderStatus.DELIVERED.toString()));
+            // orderRepos.save(new OrderEntity(Calendar.getInstance().getTime(),"Marthe Jerson", OrderStatus.CANCELLED.toString()));
+            // orderRepos.save(new OrderEntity(Calendar.getInstance().getTime(),"Xerxes Lapham", OrderStatus.SHIPPED.toString()));
+            // orderRepos.save(new OrderEntity(Calendar.getInstance().getTime(),"Brandice Loughhead", OrderStatus.ACCEPTED.toString()));
+            orderRepos.save(new OrderEntity(Calendar.getInstance().getTime(),"Brandice Loughhead", OrderStatus.PENDING.toString()));
+            productRepos.save(new ProductEntity("TV Set", 300, "https://cdn.pixabay.com/photo/2014/04/03/10/32/tv-310801_960_720.png"));
+            productRepos.save(new ProductEntity("Game Console", 200, "https://cdn.pixabay.com/photo/2020/05/15/08/28/switch-5172817_960_720.png"));
+            productRepos.save(new ProductEntity("Sofa", 100, "https://cdn.pixabay.com/photo/2013/07/12/13/58/settee-147701_960_720.png"));
+            productRepos.save(new ProductEntity("Icecream", 5, "https://cdn.pixabay.com/photo/2020/01/01/10/05/sweet-4733259_960_720.png"));
+            productRepos.save(new ProductEntity("Beer", 3, "https://cdn.pixabay.com/photo/2017/01/21/21/15/beer-1998293_960_720.jpg"));
+            productRepos.save(new ProductEntity("Phone", 500, "https://cdn.pixabay.com/photo/2014/08/05/10/27/iphone-410311_960_720.jpg"));
+            // orderProductRepos.save(new OrderProductEntity(1L, 3L, 8));
+            // orderProductRepos.save(new OrderProductEntity(2L, 6L, 1));
+            // orderProductRepos.save(new OrderProductEntity(3L, 7L, 10));
+            // orderProductRepos.save(new OrderProductEntity(4L, 11L, 3));
+            // orderProductRepos.save(new OrderProductEntity(5L, 10L, 20));
         };
     }
 }
